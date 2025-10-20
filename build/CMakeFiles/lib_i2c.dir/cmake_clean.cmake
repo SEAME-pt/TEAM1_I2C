28a2a9a1d309deeb0ac27c7b703886540cedddab
joao41 <joao41-second@users.noreply.github.com>
2025-10-20 16:16:58 +0100

    git ignore

---
+++ build/CMakeFiles/lib_i2c.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lib_i2c.dir/srcs/I2c.cpp.o"
+  "CMakeFiles/lib_i2c.dir/srcs/I2c.cpp.o.d"
+  "CMakeFiles/lib_i2c.dir/srcs/I2c_INA219.cpp.o"
+  "CMakeFiles/lib_i2c.dir/srcs/I2c_INA219.cpp.o.d"
+  "CMakeFiles/lib_i2c.dir/srcs/I2c_PcA9685.cpp.o"
+  "CMakeFiles/lib_i2c.dir/srcs/I2c_PcA9685.cpp.o.d"
+  "liblib_i2c.a"
+  "liblib_i2c.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lib_i2c.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/testes.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/testes.dir/srcs/I2c.cpp.o"
+  "CMakeFiles/testes.dir/srcs/I2c.cpp.o.d"
+  "CMakeFiles/testes.dir/srcs/I2c_INA219.cpp.o"
+  "CMakeFiles/testes.dir/srcs/I2c_INA219.cpp.o.d"
+  "CMakeFiles/testes.dir/srcs/I2c_PcA9685.cpp.o"
+  "CMakeFiles/testes.dir/srcs/I2c_PcA9685.cpp.o.d"
+  "CMakeFiles/testes.dir/test/main.cpp.o"
+  "CMakeFiles/testes.dir/test/main.cpp.o.d"
+  "testes"
+  "testes.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/testes.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
