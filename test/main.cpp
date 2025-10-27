90d3b7bddb3d94dccfc275fb1945697648472497
joao41 <joao41-second@users.noreply.github.com>
2025-10-27 09:11:35 +0000

    add test the motor

--- test/main.cpp
+++ test/main.cpp
@@ -8,7 +8,7 @@ int main()
 
 	std::cout << "set servo in 150 angle and the motors in 50% the power " << std::endl;	
 	I2c::set_servo_angle(150);
-	//I2c::motor(0,50,1);	
+	I2c::motor(0,50,1);	
 	sleep(3);
 	std::cout << "set servo in 90 angle and the motor 1 in 50% the power " << std::endl;	
 	I2c::set_servo_angle(90);
@@ -17,16 +17,10 @@ int main()
 	sleep(3);
 	std::cout << "the motor 2 in 50% the power " << std::endl;	
 	I2c::stop_motors();
-	I2c::motor(2,50,1);	
-	I2c::print();
-	std::cout << "the motors in 50% the power and invert rotacion " << std::endl;	
-	
+	I2c::motor(2,50,0);	
+	std::cout << "the motors in 50% the power and invert rotacion " << std::endl;		
 	I2c::motor(0,100,0);	
-
 	I2c::brake_motor();
-
-
-
 	I2c::end_motor_use();
 
 	
